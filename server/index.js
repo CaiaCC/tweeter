69dabd65323989145792f1c8df536eaaf898dbdb
Caia Chuang <caiachung@gmail.com>
2020-08-11 00:47:32 +0000

    add external font

--- server/index.js
+++ server/index.js
@@ -30,5 +30,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log("Caia's Tweeter app is listening on port " + PORT);
 });
